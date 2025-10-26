9f7768e14c975e9a8bd8092f9f2b7b725400d84a
shirshadip <artistavra@gmail.com>
2025-10-26 22:04:16 +0530

    practicing loops

---
+++ loops/quick-quiz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+    int i =0;
+    while (i<20+1)
+    {
+        /* code */
+        if (i>=10){
+
+            printf("the value of i is --> %d\n",i);
+        }
+        i++;
+    }
+    
+    return 0;  // End of program
+}
---
+++ loops/while-loop.c
@@ -0,0 +1,24 @@
+// #include <stdio.h>
+
+// int main(void) {
+    
+//     int i = 0;
+//     while(i<4){
+//         printf("happy birthday!\n");
+//         // i =i+1;
+//         i++; // it is incrementing operator
+//     }
+//     return 0;  // End of program
+// }
+
+#include <stdio.h>
+
+int main(void) {
+    int i = 0;
+    while (i<10){
+        printf("the value of i is --> %d \n",i);
+        i++;
+    }
+
+    return 0;  // End of program
+}
